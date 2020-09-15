048996086f21118d1c7fd2bc3dbf14e4f5d37053
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-09-15 13:03:41 -0500

    Implementing navigation

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
-const CoinsScreen = () => {
+const CoinsScreen = ({ navigation }) => {
+  const handlePress = () => {
+    navigation.navigate('CoinDetail');
+  };
+
   return (
-    <View>
-      <Text>Coins Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Coins Screen</Text>
+      <Pressable onPress={handlePress} style={styles.button}>
+        <Text style={styles.buttonText}>Go to Detail</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default CoinsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    color: 'white',
+    alignSelf: 'center',
+  },
+  button: {
+    padding: 8,
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    margin: 16,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
